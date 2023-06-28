33739a1742037090563bec46145ea9909f7d7a66
bokeum <43128571+BokEumEom@users.noreply.github.com>
2023-06-28 10:18:02 +0900

    Update locals.tf

--- aws/cloudwatch_alarms/locals.tf
+++ aws/cloudwatch_alarms/locals.tf
@@ -1,8 +1,8 @@
 locals {
   all_ec2 = {
     water-prod-app-tomcat01 = {
-      InstanceId   = "i-076e85d44c1cfd980"
-      ImageId      = "ami-0a0064415cdedc552"
+      InstanceId   = "i-xxxxxx"
+      ImageId      = "ami-xxxxxx"
       InstanceType = "t3.small"
       device       = "nvme1n1"
       fstype       = "xfs"
@@ -13,8 +13,8 @@ locals {
     }
 
     water-prod-app-tomcat02 = {
-      InstanceId   = "i-0b68772a13cc26b13"
-      ImageId      = "ami-06210da7f5863a272"
+      InstanceId   = "i-xxxxxxx"
+      ImageId      = "ami-xxxxx"
       InstanceType = "t3.small"
       device       = "nvme1n1"
       fstype       = "xfs"
